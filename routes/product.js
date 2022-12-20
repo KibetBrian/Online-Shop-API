c0f72e36452fa7fef7a7a4ba04ffc948bbf1211e
KibetBrian <briankibet2040@gmail.com>
2022-12-20 14:01:45 +0300

    reformat

--- routes/product.js
+++ routes/product.js
@@ -3,80 +3,65 @@ const Product = require('../models/Product')
 const { verifyTokenAndAdmin } = require('./tokenVerification')
 
 //Add a product
-router.post('/add', verifyTokenAndAdmin, async(req, res)=>
-{
+router.post('/add', verifyTokenAndAdmin, async (req, res) => {
     const newProduct = new Product(req.body);
-    try
-    {
+    try {
         const saveProduct = await newProduct.save();
         res.status(200).json('New Product Added')
     }
-    catch(err)
-    {
+    catch (err) {
         res.status(200).json(err)
     }
 });
+
 //Update Product
-router.put('/update/:id', verifyTokenAndAdmin, async(req, res)=>
-{
-    try
-    {
-        const  updatedProduct = await Product.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
-        res.status(200).json(updatedProduct); 
+router.put('/update/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updatedProduct);
     }
-    catch(err)
-    {
+    catch (err) {
         res.status(500).json(err)
     }
-})
+});
+
 //Delete product
-router.delete('/delete/:id', verifyTokenAndAdmin, async (req, res)=>
-{
-    try 
-    {
+router.delete('/delete/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
         const deleteProduct = await Product.findByIdAndDelete(req.params.id);
         res.status(200).send('Product Deleted Successfully')
-    }catch(err)
-    {
+    } catch (err) {
         res.status(500).json(err)
     }
-})
+});
+
 //Get product
-router.get('/product/:id', async (req, res)=>
-{
-    try 
-    {
+router.get('/product/:id', async (req, res) => {
+    try {
         const product = await Product.findById(req.params.id);
         res.status(200).json(product)
-    }catch(err)
-    {
+    } catch (err) {
         res.status(500).json(err)
     }
-})
+});
+
 //Get all products
-router.get('/allproducts', async (req, res)=>
-{
+router.get('/allproducts', async (req, res) => {
     const queryNew = req.query.new;
     const queryCategory = req.query.category;
 
-    try
-    {
+    try {
         let products;
-        if(queryNew)
-        {
-            products = await Product.find().sort({createdAt: -1}).limit(5)
-        }else if (queryCategory) 
-        {
-            console.log(queryCategory)
-            products = await Product.find({category: { $in: [queryCategory] }})
-        }else
-        {
+        if (queryNew) {
+            products = await Product.find().sort({ createdAt: -1 }).limit(5)
+        } else if (queryCategory) {
+            products = await Product.find({ category: { $in: [queryCategory] } })
+        } else {
             products = await Product.find();
         }
-        res.status(200).json(products)
+        res.status(200).json(products);
     }
-    catch(err)
-    {
+    catch (err) {
         res.status(500).json(err)
     }
 })
