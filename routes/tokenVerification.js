e7310cc681bb54302b88496833aa3b849f3bab13
KibetBrian <briankibet2040@gmail.com>
2022-12-20 13:55:31 +0300

    update

--- routes/tokenVerification.js
+++ routes/tokenVerification.js
@@ -1,53 +1,43 @@
-const jwt = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next)=>
-{
+const verifyToken = (req, res, next) => {
+
     const authHeader = req.headers.token;
-    if(authHeader)
-    {
-        const accessToken = authHeader.split(" ")[1];
-        jwt.verify(accessToken, process.env.PRIVATE_KEY, (err, user)=>
-        {
-            if (err)
-            {
-                res.status(403).json('Invalid token')
-            }else 
-            {
-                req.user = user;
-                next();
-            }
-        })
-    }else
-    {
-        res.status(401).json('You are not authenticated')
-    }
 
+    if (!authHeader) {
+        return res.status(401).json("Token not found")
+    };
+
+    const accessToken = authHeader.split(" ")[1];
+    jwt.verify(accessToken, process.env.PRIVATE_KEY, (err, user) => {
+        if (err) {
+            return res.status(403).json('Invalid token');
+        } else {
+            req.user = user;
+            next();
+        }
+    }
+    )
 }
-const verifyTokenAndAuthorization = (req, res, next)=>
-{
-    verifyToken(req, res, ()=>
-    {
-        if(req.user.id === req.params.id || req.user.admin)
-        {
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.admin) {
             next()
-        }else
-        {
+        } else {
             res.status(403).json('You are not authorized')
         }
     })
 }
 
-const verifyTokenAndAdmin = (req, res, next)=>
-{
-    verifyToken(req, res, ()=>
-    {
-        if(req.user.admin)
-        {
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.admin) {
             next();
-        }else 
-        {
+        } else {
             res.status(403).json('You do not have permission')
         }
     })
-}
+};
+
 module.exports = { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin }
