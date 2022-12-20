54c7675547177908a5912c878af02d1af90005e4
KibetBrian <briankibet2040@gmail.com>
2022-12-20 13:55:20 +0300

    update

--- routes/user.js
+++ routes/user.js
@@ -4,83 +4,72 @@ const User = require('../models/User');
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./tokenVerification');
 
 //User Update
-router.put('/update/:id', verifyTokenAndAuthorization, async (req, res)=>
-{
-   if (req.body.password)
-   {
-    //bcrypt
-    const saltRounds = 10;
-    const plainTextPassword = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    req.body.password = await bcrypt.hash(plainTextPassword, salt);
-   }
-   try
-   {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
+router.put('/update/:id', verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        //bcrypt
+        const saltRounds = 10;
+        const plainTextPassword = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(plainTextPassword, salt);
+    }
 
-    const {password, ...others} = updatedUser._doc;
-    res.status(200).json(others)
-   }
-   catch(err)
-   {
-       res.status(500).json(err);
-   }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+
+        const { password, ...otherProperties } = updatedUser._doc;
+        res.status(200).json(otherProperties)
+    }
+    catch (err) {
+        res.status(500).json("Internal server error");
+    }
 })
 
 //Delete Account
-router.delete('/delete', verifyTokenAndAuthorization, async (req, res)=>
-{
-    try 
-    {
+router.delete('/delete', verifyTokenAndAuthorization, async (req, res) => {
+    try {
         const deleteUser = await User.findByIdAndDelete(req.user.id);
         res.status(200).send('User Deleted Successfully')
-    }catch(err)
-    {
+    } catch (err) {
         res.status(500).json(err)
     }
 })
 //Get User 
-router.get('/getuser/:id', verifyTokenAndAdmin, async (req, res)=>
-{
-    try 
-    {
+router.get('/getuser/:id', verifyTokenAndAdmin, async (req, res) => {
+    try {
         const user = await User.findById(req.params.id);
-        const {password, ...others} = user._doc;
-        res.status(200).json(others)
-    }catch(err)
-    {
+        const { password, ...otherProperties } = user._doc;
+        res.status(200).json(otherProperties)
+    } catch (err) {
         res.status(500).json(err)
     }
-})
-//getAllUsers 
+});
 
-router.get('/allUsers', verifyTokenAndAdmin, async (req, res)=>
-{
-    try 
-    {
+//getAllUsers 
+router.get('/allUsers', verifyTokenAndAdmin, async (req, res) => {
+    try {
         const users = await User.find();
-        res.status(200).json(users)
+        res.status(200).json(users);
     }
-    catch(err)
-    {
-        res.status(500).json(err) 
+    catch (err) {
+        console.log("Error", err);
+        res.status(500).json("Internal server error");
     }
-})
+});
+
 //User stats
-router.get('/stats', verifyTokenAndAdmin, async (req, res)=>{
+router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
     const date = new Date();
     const lastYear = new Date(date.setFullYear(date.getFullYear) - 1);
-    try 
-    {
+    try {
         const data = await User.aggregate([
-            {$match: { createdAt: {$gte: lastYear}}},
-            {$project: {month: {$month: "$createdAt"}}},
-            {$group: {_id: "$month", total: {$sum: 1}}}
+            { $match: { createdAt: { $gte: lastYear } } },
+            { $project: { month: { $month: "$createdAt" } } },
+            { $group: { _id: "$month", total: { $sum: 1 } } }
         ])
         res.status(200).json(data)
-    }catch(err)
-    {
+    } catch (err) {
         res.status(500).json(err)
     }
-})
+});
+
 module.exports = router;
