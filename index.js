03a37baf9d890004ab4a29fcc3ce9274b58f570f
KibetBrian <briankibet2040@gmail.com>
2022-12-20 14:01:27 +0300

    reformat

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require ('express');
-const app = express();
-const mongoose = require ('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 const userRoute = require('./routes/user');
-const authRoute = require ('./routes/auth');
+const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
@@ -10,18 +9,11 @@ const paypalRoute = require('./routes/paypal');
 require('dotenv').config()
 app.use(express.json());
 
+const app = express();
+const PORT = process.env.PORT || 8080;
+
 //Mongo DB Connection
-mongoose.connect
-(
-    process.env.MONGODB_CONNECT
-).then(()=>
-{
-    console.log('Database Connected');
-}
-).catch((err)=>
-{
-    console.log(err)
-})
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Database connected")).catch((err) => console.log(err));
 
 //Routes 
 app.use('/api/user/', userRoute);
@@ -31,9 +23,6 @@ app.use('/api/cart', cartRoute);
 app.use('/api/orders/', orderRoute);
 app.use('/api/payment/', paypalRoute);
 
-
-//Port listening
-app.listen(8080, ()=>
-{
+app.listen(PORT, () => {
     console.log('Server Connected')
 })
